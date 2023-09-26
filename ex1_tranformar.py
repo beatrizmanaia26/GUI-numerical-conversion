93ba1c0325728977a9517630620ae176b34e40f9
Beatriz Manaia Lourenço Berto <130764598+beatrizmanaia26@users.noreply.github.com>
2023-09-26 19:30:47 -0300

    Add files via upload

---
+++ ex1_tranformar.py
@@ -0,0 +1,10 @@
+from tkinter import * 
+
+janela = Tk()
+janela.geometry("400x400")
+
+texto = Label(janela, text="Conversão Numérica") 
+texto.place(x=50,y=100, anchor=CENTER) 
+
+n1 = Label(janela, text="Entre o primeiro número:") 
+n1.place(x=50,y=100, anchor=CENTER) 
